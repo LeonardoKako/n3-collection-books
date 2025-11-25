d580510c15cca09fbe236fc6611a35047627c330
LeonardoKako <fleithkako@gmail.com>
2025-11-25 19:35:27 -0300

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // remove chaves que nn estao no dto
+      forbidNonWhitelisted: true, // levanta erro ao notar chaves que nn estao no dto
+      transform: false, // tenta transformar os tipos de dados de param para dtos
+    }),
+  );
+
+  await app.listen(process.env.PORT ?? 3000);
+}
+bootstrap();
